fixed dt_feat normalization bug

SimpleSAC/conservative_sac_main.py
@@ -16,7 +16,7 @@ import absl.flags
 from .conservative_sac import ConservativeSAC
 from .replay_buffer import batch_to_torch, subsample_batch_n, subsample_flat_batch_n, load_dataset, load_d4rl_dataset
 from .model import TanhGaussianPolicy, FullyConnectedQFunction, SamplerPolicy
-from .sampler import StepSampler, TrajSampler
+from .sampler import TrajSampler
 from .utils import *
 from viskit.logging import logger, setup_logger
 from dau.code.envs.biped import Walker
@@ -47,17 +47,15 @@ FLAGS_DEF = define_flags_with_default(
     eval_n_trajs=5,
     load_model='',
     visualize_traj=False,
-    N_steps=80,
-    # N_steps=.08,
-    N_datapoints=250000,
-    dt_feat=False,
+    N_steps=.02,
+    # N_datapoints=250000,
+    dt_feat=True,
     use_pretrained_q_target=False,
     pretrained_target_path='',
-    shared_q_target=False,
+    shared_q_target=True,
     max_q_target=False,
     # use_pretrained_q_target=True,
     # pretrained_target_path='/iris/u/kayburns/continuous-rl/CQL/experiments/.02/aec001f95d094fa598456707e8c81814/',
-    # shared_q_target=True,
 
     cql=ConservativeSAC.get_default_config(),
     logging=WandBLogger.get_default_config(),
@@ -104,10 +102,6 @@ def main(argv):
         #     dataset['rewards'] = dataset['rewards'] * FLAGS.reward_scale + FLAGS.reward_bias
         #     datasets[dt] = dataset
     elif "pendulum_" in FLAGS.env:
-        dt = float(FLAGS.env.split('_')[1])
-        env = gym.make('Pendulum-v1').unwrapped
-        env.dt = dt
-
         datasets, eval_samplers = {}, {}
         for dt in [.01, .02, .005]:
             env = gym.make('Pendulum-v1').unwrapped
@@ -261,12 +255,12 @@ def main(argv):
                             datasets[dt], per_dataset_batch_size, max_steps)
                     if FLAGS.dt_feat:
                         dt_feat = np.ones((per_dataset_batch_size, max_steps, 1))*dt
-                        dt_feat = (dt_feat - np.mean(dts)) / np.std(dts)
+                        norm_dt = (dt_feat - np.mean(dts)) / np.std(dts)
                         batch_dt['observations'] = np.concatenate([
-                            batch_dt['observations'], dt_feat], axis=2
+                            batch_dt['observations'], norm_dt], axis=2
                         ).astype(np.float32)
                         batch_dt['next_observations'] = np.concatenate([
-                            batch_dt['next_observations'], dt_feat], axis=2
+                            batch_dt['next_observations'], norm_dt], axis=2
                         ).astype(np.float32)
                     batch_dts.append(batch_dt)
 
@@ -275,11 +269,12 @@ def main(argv):
                 for k in batch_dts[0].keys():
                     batch[k] = np.concatenate([b[k] for b in batch_dts], axis=0)
                 batch = batch_to_torch(batch, FLAGS.device)
-                n_steps = torch.Tensor([FLAGS.N_steps/dt for dt in dts])
-                # n_steps = torch.Tensor([1 for dt in dts])
+                # n_steps = torch.Tensor([FLAGS.N_steps/dt for dt in dts])
+                n_steps = torch.Tensor([1 for dt in dts])
                 n_steps = n_steps.repeat_interleave(per_dataset_batch_size)
                 # TODO weird: this is replicating the same indexing per_dataset_batch_size times
                 if FLAGS.shared_q_target:
+                    # batch['next_observations'][:,(n_steps-1).long(),-1] = max(dts)
                     batch['next_observations'][:,(n_steps-1).long(),-1] = (max(dts) - np.mean(dts)) / np.std(dts)
                 metrics.update(prefix_metrics(sac.train(batch, n_steps, FLAGS.max_q_target), 'sac'))
 
@@ -291,8 +286,9 @@ def main(argv):
                     output_file = os.path.join(
                         wandb_logger.config.output_dir, f'eval_dt_{dt}_{epoch}.gif')
 
+                    norm_dt = (dt - np.mean(dts)) / np.std(dts)
                     trajs = eval_sampler.sample(
-                        sampler_policy, FLAGS.eval_n_trajs, FLAGS.dt_feat,
+                        sampler_policy, FLAGS.eval_n_trajs, FLAGS.dt_feat, norm_dt,
                         deterministic=True, video=video, output_file=output_file
                     )
 
@@ -306,9 +302,10 @@ def main(argv):
                             metrics['hip1'] = wandb_logger.plot(mean_actions[:,2])
                             metrics['knee1'] = wandb_logger.plot(mean_actions[:,3])
                         elif "pendulum_" in FLAGS.env:
+                            norm_dt = (dt - np.mean(dts)) / np.std(dts)
                             generate_pendulum_visualization(
                                 sac.policy, sac.qf1, sac.qf2, wandb_logger,
-                                f'val_dt{dt}_epoch{epoch}.png', FLAGS.dt_feat, dt)
+                                f'val_dt{dt}_epoch{epoch}.png', FLAGS.dt_feat, norm_dt)
 
                     if "goal-observable" in FLAGS.env:
                         metrics[f'max_success_{dt}'] = np.mean([np.max(t['successes']) for t in trajs])

SimpleSAC/utils.py
@@ -208,7 +208,7 @@ def arr_to_th(arr) -> torch.Tensor:
 
     return torch.from_numpy(arr).float().to('cuda')
 
-def generate_pendulum_visualization(policy, qf1, qf2, logger, filename, dt_feat, dt=.02):
+def generate_pendulum_visualization(policy, qf1, qf2, logger, filename, dt_feat, dt):
     nb_pixels = 50
     theta_space = np.linspace(-np.pi, np.pi, nb_pixels)
     dtheta_space = np.linspace(-10, 10, nb_pixels)